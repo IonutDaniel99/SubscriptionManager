3ab50e0797cb2c6362a1ce87823d2ea33ceac422
Daniel Ene <ionut.daniel644@gmail.com>
2024-08-21 10:21:12 +0300

    authscreen and login

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import React, { useEffect, useState } from 'react'
 import type { ReactNode } from 'react'
 import { SafeAreaView, StatusBar, Text, useColorScheme, View } from 'react-native'
 import RootNavigation from './Components/Navigation/RootNavigator'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { GoogleSignin } from '@react-native-google-signin/google-signin'
-import auth from '@react-native-firebase/auth'
 
 const App: () => ReactNode = () => {
     const isDarkMode = useColorScheme() === 'dark'
--- src/Pages/Auth/AuthScreen.tsx
+++ src/Pages/Auth/AuthScreen.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
-import { Button, Text, ToastAndroid, View } from 'react-native'
-import { currentUserAtom } from '../../atoms/useCurrentUserAtom'
-import { useAtom } from 'jotai'
+import { Button, Image, Text, ToastAndroid, View } from 'react-native'
 import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin'
-import { GoogleSingInConfigs } from '../../configs/GoogleSingInConfig'
-import { CustomShowToast } from '../../Components/Toast/ToastComponent'
 import auth from '@react-native-firebase/auth'
 
 export default function AuthScreen() {
@@ -25,14 +21,28 @@ export default function AuthScreen() {
 
     return (
         <View>
+            <View className="w-full h-screen grid-cols-2 lg:grid lg:min-h-screen">
+                <View className="flex items-center justify-center h-1/6 bg-primary lg:h-full">
+                    <View className="flex items-center max-w-md gap-8 text-center lg:space-y-4 text-primary-foreground lg:block">
+                        <Text className="text-2xl font-bold lg:text-5xl text-foreground">Welcome Back!</Text>
+                        <Text className="hidden lg:block lg:text-xl text-foreground">
+                            Sign in to your account and start exploring our local events.
+                        </Text>
+                    </View>
+                </View>
+                <View className="flex items-center justify-center h-5/6 lg:h-full ">
+                    <View className="w-full max-w-md px-6 mx-auto space-y-6 lg:px-2">
+                        <GoogleSigninButton
+                            style={{ width: 192, height: 48 }}
+                            size={GoogleSigninButton.Size.Wide}
+                            color={GoogleSigninButton.Color.Dark}
+                            onPress={() => onGoogleButtonPress()}
+                        />
+                        <Button title="annon" onPress={() => loginAnonymous()} />
+                    </View>
+                </View>
+            </View>
             <Text>Login</Text>
-            <GoogleSigninButton
-                style={{ width: 192, height: 48 }}
-                size={GoogleSigninButton.Size.Wide}
-                color={GoogleSigninButton.Color.Dark}
-                onPress={() => onGoogleButtonPress()}
-            />
-            <Button title="annon" onPress={() => loginAnonymous()} />
         </View>
     )
 }
